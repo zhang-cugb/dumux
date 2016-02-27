f2fc40096120fe79af29513c5b176d3c5f2d35c2
Timo Koch <timo.koch@iws.uni-stuttgart.de>
2016-02-27 12:02:58 +0100

    Remove element volume variables. There are not needed anymore.

--- dumux/implicit/cellcentered/propertydefaults.hh
+++ dumux/implicit/cellcentered/propertydefaults.hh
@@ -60,8 +60,8 @@ SET_TYPE_PROP(CCModel, DofMapper, typename GET_PROP_TYPE(TypeTag, ElementMapper)
 //! Set the BaseLocalResidual to CCLocalResidual
 SET_TYPE_PROP(CCModel, BaseLocalResidual, Dumux::CCLocalResidual<TypeTag>);
 
-//! An array of secondary variable containers
-SET_TYPE_PROP(CCModel, ElementVolumeVariables, Dumux::CCElementVolumeVariables<TypeTag>);
+//! The stencil container
+SET_TYPE_PROP(CCModel, Stencils, Dumux::CCStencils<TypeTag>);
 
 //! Assembler for the global jacobian matrix
 SET_TYPE_PROP(CCModel, JacobianAssembler, Dumux::CCAssembler<TypeTag>);
