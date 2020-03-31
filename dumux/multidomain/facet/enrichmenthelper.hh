04e7412acd18f5c841246533b6f1cb0d187cf961
Melanie Lipp <melanie.lipp@iws.uni-stuttgart.de>
2020-03-31 14:59:11 +0200

    Add missing backslashes for copydoc.

--- dumux/multidomain/facet/enrichmenthelper.hh
+++ dumux/multidomain/facet/enrichmenthelper.hh
@@ -20,7 +20,7 @@
 /*!
  * \file
  * \ingroup FacetCoupling
- * \brief copydoc Dumux::VertexEnrichmentHelper
+ * \brief \copydoc Dumux::VertexEnrichmentHelper
  */
 #ifndef DUMUX_VERTEX_ENRICHMENT_HELPER_HH
 #define DUMUX_VERTEX_ENRICHMENT_HELPER_HH
